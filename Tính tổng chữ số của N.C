780f1d78891202e8f0db77dedef8cc94f21e9600
locnguyenlaptrinh <locn0920@gmail.com>
2026-03-16 13:53:26 +0700

    Create Tính tổng chữ số của N.C

---
+++ Tính tổng chữ số của N.C
@@ -0,0 +1,29 @@
+Nhập vào n, tính tổng các chữ số của n, và in ra kết quả
+Input 
+12341
+Output 
+Copy
+11
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+typedef long long ll;
+typedef float f;
+typedef double d;
+int main(){
+ll n;
+scanf("%lld",&n);
+ll s=0;
+while(n!=0){
+s+=n%10;
+n/=10;
+}
+printf("%lld",s);
+return 0;
+}
+
+
+
+
+
+
